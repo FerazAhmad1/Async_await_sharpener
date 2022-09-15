eb3100ebed8b8f1f16a23d4b19a37adb711c3e34
FerazAhmad1 <ferazahmadgit62@gmail.com>
2022-09-15 17:15:39 +0530

    promises

--- script.js
+++ script.js
@@ -79,3 +79,33 @@ const promiseWifeBringingTickets = new Promise((resolve, reject) => {
     resolve("ticket");
   }, 3000);
 });
+
+const getPopCorn = promiseWifeBringingTickets.then((t) => {
+  console.log(`wife:here is ticket`);
+  console.log(`husband:we should go in`);
+  console.log(`wife:no i am hungry`);
+  return new Promise((resolve, reject) => {
+    resolve(`${t} popcorn`);
+  });
+});
+
+const goGetButter = getPopCorn.then((t) => {
+  console.log(`husband:here is popcorn`);
+  console.log(`husband:we should go in`);
+  console.log(`wife:no,i need butter on my popcorn`);
+  return new Promise((resolve, reject) => {
+    resolve(`${t} butter`);
+  });
+});
+
+const getColdDrink = goGetButter.then((t) => {
+  console.log(`husband:here is butter`);
+  console.log(`husband:we should go in`);
+  console.log(`wife:no,i need thumsup`);
+  return new Promise((resolve, reject) => {
+    resolve(`${t} thumsUp`);
+  });
+});
+getColdDrink.then((t) => console.log(t));
+console.log("person4:shows ticket");
+console.log("person5:shows ticket");
